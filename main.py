e13163440a3588008e547e4fc8d3469d29654b7e
Esketit! <37747855+The-Big-Banana@users.noreply.github.com>
2021-10-13 02:25:13 -0400

    sleep

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 radius_X = []
 radius_Y = []
 radius_Z = 2.4
+
+# Append radii
+for i in range(0, 185, 5):
+    radius_X.append(1.2*np.cos(np.radians(i)))
+    radius_Y.append(1.2*np.sin(np.radians(i)))
